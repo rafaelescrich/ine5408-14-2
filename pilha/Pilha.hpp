c7e6e2417646940a8514ef95d5435046b860defa
Rafael Escrich <rafaelescrich@gmail.com>
2014-08-26 18:15:13 -0300

    added pilha folder and Pilha.hpp file

---
+++ pilha/Pilha.hpp
@@ -0,0 +1,16 @@
+template<typename T>
+class Pilha {
+private:
+	
+
+public:
+    Pilha()
+    Pilha<T>(int t)
+	void empilha(T dado)
+	T desempilha()
+	T topo()
+	int getPosTopo()
+	void limparPilha()
+	bool PilhaVazia()
+	bool PilhaCheia()
+};
