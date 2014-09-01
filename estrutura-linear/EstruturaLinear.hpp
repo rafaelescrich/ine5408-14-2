7cf48f31afb45086dbf031de49462e8a3b1b1541
Rafael Escrich <rafaelescrich@gmail.com>
2014-08-31 21:50:27 -0300

    finished the code

--- estrutura-linear/EstruturaLinear.hpp
+++ estrutura-linear/EstruturaLinear.hpp
@@ -1,29 +1,87 @@
-#include<iostream> 
+#include <iostream>
+
+#define MAXESTRUTURA 100
 
 template <typename T> class EstruturaLinear {
     
     private:
-        
-        int topo;                       //variável privada que indica o topo
-        
-        int tam;                        //variável privada que indica o tamanho
 
+        int topo;
+
+        T elementos[MAXESTRUTURA];
+    
     public:
+
+        EstruturaLinear() {             //Construtor
         
-        EstruturaLinear();              //Construtor
+            topo = -1;
+
+        }
+
+        EstruturaLinear<T>(int tam) {   //Construtor
+        
+            T elementos[tam];
+            topo = -1;
+
+        }
+
+        void inclui(T dado) {           //método para incluir dado de qualquer tipo
+        
+            if (estruturaCheia()) {
+                throw ("Estrutura Cheia");
+            }
+            else {
             
-        EstruturaLinear<T>(int tam);    //Construtor
+                topo = topo + 1;
+                elementos[topo] = dado;
+            }
+        }
+
+        T retira() {                    //método para retirar elemento da estrutura
         
-        void inclui(T dado);            //método para incluir dado de qualquer tipo
+            if (estruturaVazia()) {
+                throw ("Estrutura Vazia");
+            }
+            else {
+            
+                topo = topo - 1;
+                return elementos[topo+1];
+            }
+
+        }
+
+        T ultimo() {                    //método para retornar último elemento
         
-        T retira();                     //método para retirar elemento da estrutura
-        
-        T ultimo();                     //método para retornar último elemento
-        
+            if (estruturaVazia()) {
+                throw ("Estrutura Vazia");
+            }
+            else {
+            
+                return elementos[topo];
+            }
+        }
+
         int getUltimo();                //método que retorna posição do útlimo elemento
         
-        bool estruturaCheia();          //método que retorna true para estrutura cheia
+        bool estruturaCheia() {         //método que retorna true para estrutura cheia
+            
+            if (topo == MAXESTRUTURA -1)
+                return true;
+            return false;
+        }
         
-        bool estruturaVazia();          //método que retorna true para estrutura vazia
-
+        bool estruturaVazia() {          //método que retorna true para estrutura vazia
+            
+            if (topo == -1)
+                return true;
+            return false;
+        }
 };
+
+int main() {
+
+    EstruturaLinear<int> intEstrutura;
+    intEstrutura.inclui(7);
+    std::cout << intEstrutura.ultimo() << std::endl;
+    return 0;
+}
